6e818661804844aa7e7025c06a263bf5b91d49d0
killgriff22 <kilgrif22033@gmail.com>
2024-08-24 15:18:09 -0400

    test 1

---
+++ main.py
@@ -0,0 +1,12 @@
+from MultiTerm import *
+termsize = os.get_terminal_size()
+termsize = (termsize.columns, termsize.lines)
+SCREEN = Screen(termsize,(0,0))
+Ground = canvas((termsize[0],4))
+Ground.fill("█")
+SCREEN.blit(str(Ground),(0,termsize[1]-4),Fore.GREEN,RESET)
+particles = []
+
+while True:
+    SCREEN.clear()
+    SCREEN.draw()
